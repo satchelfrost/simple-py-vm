62031c2a0c32c18c485d9b7a60543279ca19874b
Reese Gallagher <re325479@ucf.edu.com>
2024-04-15 13:15:55 -0400

    Minor changes

--- pyvm.py
+++ pyvm.py
@@ -38,6 +38,7 @@ class Opcode(Enum):
     TRUE       = 27
     FALSE      = 28
     ASSERT     = 29
+    # TODO local variables
 
 binops = {
     Opcode.ADD     : lambda a, b: a +   b,
@@ -287,7 +288,6 @@ class Compiler:
         self.chunk.code.append(idx)
         return idx
 
-
 class Result(Enum):
     OK          = 0
     COMPILE_ERR = 1
@@ -360,7 +360,6 @@ class VM:
                     continue
                 case Opcode.FALSE:
                     self.stack.append(False)
-                    continue
                     continue
                 case Opcode.ASSERT:
                     if not self.stack.pop():
